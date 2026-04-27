b2827b9b098a21c240d60d975cd32103c42449bc
FelixBriz7 <felixbriz077@gmail.com>
2026-04-27 09:59:18 +0200

    Actividad 4.10/ 1.- Extración de la interfaz VentanaDepart

---
+++ src/InterfaceVentanaDepart.java
@@ -0,0 +1,10 @@
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+public interface InterfaceVentanaDepart {
+
+	void actionPerformed(ActionEvent e);
+
+	void verporconsola() throws IOException;// fin verporconsola
+
+}
